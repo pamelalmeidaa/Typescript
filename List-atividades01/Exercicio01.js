894498e84c8997536d9f63c1ad6940735cadb810
Pamela <contatopamela@outlook.com>
2024-06-26 17:37:00 -0300

    Atividade 1

---
+++ List-atividades01/Exercicio01.js
@@ -0,0 +1,15 @@
+const readlineSync = require('readline-sync');
+
+//* Definindo o salário e o abono
+let salario = 10000.00;
+let abono = 1000.00;
+
+//*  Calculando o novo salário
+let novoSalario = salario + abono;
+
+//* Lendo um número do usuário
+let x = readlineSync.questionInt("Digite seu salário: ");
+console.log("O número digitado é " + x);
+
+//* Exibindo o novo salário
+console.log("Novo salário: " + novoSalario.toFixed(2));
